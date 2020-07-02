b17414326aaeffd993eee7fefda9daa213ba4eaa
xalstnx <xalstnx@naver.com>
2020-07-02 11:44:19 +0900

    Update 9086.c

--- 9086.c
+++ 9086.c
@@ -4,6 +4,7 @@ int main(void)
 {
     int num;
     char a[1001];
+    //처음에는 char *a 로 풀어서 컴퓨터에서 실행하면 정상적으로 출력이 되었는데 백준에서는 컴파일오류가 뜸
     scanf("%d", &num);
     for(int i=0;i<num;i++){
         scanf("%s", a);
