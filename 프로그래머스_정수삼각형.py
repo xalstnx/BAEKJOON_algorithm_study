565bbae51c7944d02098dfbe7760adf48d306845
xalstnx <xalstnx@naver.com>
2021-08-13 16:21:53 +0900

    정수삼각형(DP)
    [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]	30
    
    삼각형 모양의 정수들이 주어지고 위에서 아래로 내려오면서 가장 큰 값 출력

---
+++ 프로그래머스_정수삼각형.py
@@ -0,0 +1,11 @@
+def solution(triangle):
+    for t in range(1,len(triangle)):
+        for i in range(len(triangle[t])):
+            if i == 0:
+                triangle[t][0] += triangle[t-1][0]
+            elif i == t:
+                triangle[t][-1] += triangle[t-1][-1]
+            else:
+                triangle[t][i] += max(triangle[t-1][i-1], triangle[t-1][i])
+                
+    return max(triangle[len(triangle)-1])
